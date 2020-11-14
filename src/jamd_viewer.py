b96920e020a7a83ad92fc05635dbd8729cdaa362
Javier Moragon <jamofer@gmail.com>
2020-11-14 02:47:38 +0100

    Script launcher

--- src/jamd_viewer.py
+++ src/jamd_viewer.py
@@ -1,5 +1,4 @@
 import os
-from argparse import ArgumentParser
 
 from flask import Flask
 from flask_desktop_ui import FlaskDesktopUI
@@ -22,15 +21,3 @@ def run(path):
         return markdown_parser.to_html(markdown_contents)
 
     FlaskDesktopUI(app).run()
-
-
-def parse_arguments():
-    arguments = ArgumentParser()
-    arguments.add_argument('path', help='Markdown file to be opened')
-
-    return arguments.parse_args()
-
-
-if __name__ == '__main__':
-    arguments = parse_arguments()
-    run(arguments.path)
